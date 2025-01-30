6280b4f36078090bb0106f92bebb48d307bae193
karadagserhat <serhatkaradagg@outlook.com>
2025-01-30 19:41:08 +0300

    updated ui

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,14 @@
+import { ShopComponent } from './features/shop/shop.component';
+import { ProductDetailsComponent } from './features/shop/product-details/product-details.component';
+import { NotFoundComponent } from './shared/components/not-found/not-found.component';
+import { ServerErrorComponent } from './shared/components/server-error/server-error.component';
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'shop', pathMatch: 'full' },
+  { path: 'shop', component: ShopComponent },
+  { path: 'shop/:id', component: ProductDetailsComponent },
+  { path: 'not-found', component: NotFoundComponent },
+  { path: 'server-error', component: ServerErrorComponent },
+  { path: '**', redirectTo: 'not-found', pathMatch: 'full' },
+];
---
+++ client/src/app/layout/header/header.component.ts
@@ -0,0 +1,52 @@
+import { Component, inject, effect, signal } from '@angular/core';
+import { MatBadge } from '@angular/material/badge';
+import { MatButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { BusyService } from '../../core/services/busy.service';
+import { MatProgressBar } from '@angular/material/progress-bar';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [
+    MatIcon,
+    MatButton,
+    MatBadge,
+    RouterLink,
+    RouterLinkActive,
+    MatProgressBar,
+  ],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss',
+})
+export class HeaderComponent {
+  busyService = inject(BusyService);
+
+  themes = {
+    lightTheme: 'light-theme',
+    darkTheme: 'dark-theme',
+  };
+
+  theme = signal<string>(this.getThemeFromLocalStorage());
+
+  ngOnInit(): void {}
+
+  getThemeFromLocalStorage() {
+    return localStorage.getItem('theme') || this.themes.lightTheme;
+  }
+
+  constructor() {
+    effect(() => {
+      document.documentElement.className = this.theme();
+      localStorage.setItem('theme', this.theme());
+    });
+  }
+
+  handleTheme() {
+    const { lightTheme, darkTheme } = this.themes;
+    const newTheme = this.theme() === lightTheme ? darkTheme : lightTheme;
+
+    this.theme.set(newTheme);
+  }
+}
